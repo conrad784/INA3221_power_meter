1184ca86e8fc5c567d2aa04ac6991f42bf6e7e88
Conrad Sachweh <conrad.sachweh@ausy-technologies.de>
2021-01-31 15:04:13 +0100

    move static lines outside of loop
    went from ~700millis to ~500millis, bigger oled seems to be impacting performance

--- power_meter.ino
+++ power_meter.ino
@@ -8,9 +8,15 @@
 // can only be used with enabled serial output
 //#define DEBUG_INA3221
 
-U8X8_SH1106_128X64_NONAME_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
+//U8X8_SH1106_128X64_NONAME_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
+U8X8_SSD1327_EA_W128128_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
 
-SDL_Arduino_INA3221 ina3221;
+
+// default is R100
+#define INA3221_ADDRESS (0x40)
+#undef SHUNT_RESISTOR_VALUE
+#define SHUNT_RESISTOR_VALUE (0.01)  // default shunt resistor value of 0.1 Ohm
+SDL_Arduino_INA3221 ina3221(INA3221_ADDRESS, SHUNT_RESISTOR_VALUE);
 // the three channels of the INA3221 named for SunAirPlus Solar Power Controller channels (www.switchdoc.com)
 const static int CHANNEL_1 = 1;
 const static int CHANNEL_2 = 2;
@@ -36,6 +42,26 @@ void setup(void)
   
   u8x8.begin();
   u8x8.setFont(u8x8_font_chroma48medium8_r);
+
+  // Static Display content
+  u8x8.drawString(0,1,"Power: ");
+  u8x8.drawString(5, 7, "V");
+  u8x8.drawString(5, 8, "V");
+  u8x8.drawString(5, 9, "V");
+
+  u8x8.drawString(5, 2, "mA");
+  u8x8.drawString(13, 2, "mAh");
+  u8x8.drawString(5, 3, "mA");
+  u8x8.drawString(13, 3, "mAh");
+  u8x8.drawString(5, 4, "mA");
+  u8x8.drawString(13, 4, "mAh");
+
+  u8x8.drawString(5, 6, "mA");
+  u8x8.drawString(13, 6, "mAh");
+
+  u8x8.drawString(13, 11, "min");
+
+
 }
 
 void loop(void)
@@ -73,11 +99,10 @@ void loop(void)
   /*
     Begin drawing to display.
    */
-  u8x8.drawString(0,1,"Power: ");
-  const char power[8];
-  dtostrf(busvoltage1*shuntvoltage1, 3, 2, power);
-  u8x8.drawString(7, 1, power);
-  u8x8.drawString(12, 1, "mW");
+  //const char power[8];
+  //dtostrf(busvoltage1*shuntvoltage1, 3, 2, power);
+  //u8x8.drawString(7, 1, power);
+  //u8x8.drawString(12, 1, "mW");
 
   print_consumption_line(current_mA1, mAh_1, 2);
   print_consumption_line(current_mA2, mAh_2, 3);
@@ -88,20 +113,26 @@ void loop(void)
 
   // last line with voltage and time
   // for me all outputs are provided with same voltage
-  const char voltage[6];
-  dtostrf(busvoltage1, 5, 2, voltage);
-  u8x8.drawString(0, 7, voltage);
-  u8x8.drawString(5, 7, "V");
+  const char voltage_1[6];
+  char voltage_2[6] = {0};
+  char voltage_3[6] = {0};
+  dtostrf(busvoltage1, 5, 2, voltage_1);
+  dtostrf(busvoltage2, 5, 2, voltage_2);
+  dtostrf(busvoltage3, 5, 2, voltage_3);
+  u8x8.drawString(0, 7, voltage_1);
+  u8x8.drawString(0, 8, voltage_2);
+  u8x8.drawString(0, 9, voltage_3);
+
 
   float runtime_minutes = float(current_millis)/1000/60;
-  const char runtime_minutes_char[6];
+  char runtime_minutes_char[6] = {0};
   dtostrf(runtime_minutes, 4, 1, runtime_minutes_char);
-  u8x8.drawString(9, 7, runtime_minutes_char);
-  u8x8.drawString(13, 7, "min");
+  u8x8.drawString(9, 11, runtime_minutes_char);
 
   // debug millis refresh rate
   #ifdef DEBUG_MILLIS
-  u8x8.drawString(0, 5, "Millis: ");
+  u8x8.clearLine(5);
+  //u8x8.drawString(0, 5, "Millis: ");
   u8x8.setCursor(9, 5);
   u8x8.print(actual_millis);
   #endif
@@ -121,15 +152,13 @@ void loop(void)
 
 void print_consumption_line(float mA, float mAh, int row)
 {
-  const char mA_char[8];
+  char mA_char[8];
   dtostrf(mA, 5, 1, mA_char);
   u8x8.drawString(0, row, mA_char);
-  u8x8.drawString(5, row, "mA");
 
-  const char mAh_char[8];
+  char mAh_char[8];
   dtostrf(mAh, 5, 1, mAh_char);
   u8x8.drawString(8, row, mAh_char);
-  u8x8.drawString(13, row, "mAh");
 }
 
 void serial_print_channel(float busvoltage, float shuntvoltage, float loadvoltage, float current, float consumption, char* channel_name)
